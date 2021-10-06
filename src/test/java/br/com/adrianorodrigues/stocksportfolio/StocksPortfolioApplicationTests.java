0a24f50eaa86cfd7f609accfc138373043f19f9b
Adriano Rodrigues <adrianomrodrigues@hotmail.com>
2021-10-06 14:28:02 -0300

    Initial commit

---
+++ src/test/java/br/com/adrianorodrigues/stocksportfolio/StocksPortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adrianorodrigues.stocksportfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksPortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
